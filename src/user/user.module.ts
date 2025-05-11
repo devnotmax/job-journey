eb2b4d17059dfe333543715c806bf8083fb3044b
devnotmax <develop.maxsj@gmail.com>
2025-05-11 20:10:34 -0300

    login y registro listo

---
+++ src/user/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [AuthModule],
+  controllers: [UserController],
+  providers: [UserService, PrismaService],
+})
+export class UserModule {}
